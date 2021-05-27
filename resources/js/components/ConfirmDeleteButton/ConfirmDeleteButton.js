a9b096abdc655cbdcdee4b17cdef74c8636eda4b
Rob Trehy <robert.trehy@outlook.com>
2021-05-27 08:31:47 +0100

    Bump Laravel Mix to v6

--- resources/js/components/ConfirmDeleteButton/ConfirmDeleteButton.js
+++ resources/js/components/ConfirmDeleteButton/ConfirmDeleteButton.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
-import PropTypes from "prop-types"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from "prop-types";
 
 const ConfirmDeleteButton = props => {
     const {
@@ -26,7 +25,7 @@ const ConfirmDeleteButton = props => {
     const onQuery = e => {
         e.preventDefault();
         setWaiting(true);
-        
+
         setTimeout(() => {
             setWaiting(false);
         }, timeout)
@@ -44,13 +43,17 @@ const ConfirmDeleteButton = props => {
 
     const trashIcon = () => {
         if (icon) {
-            return <FontAwesomeIcon icon={['fas', 'trash']} />
+            return (
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+            )
         }
         return null;
     }
 
     return (
-        waiting ? 
+        waiting ?
             <a href="#" onClick={onConfirm} className={confirmClasses}>{trashIcon()} {confirmText}</a> :
             <a href="#" onClick={onQuery} className={queryClasses}>{trashIcon()} {text}</a>
     )
