f9c0fba2a43efca82c470ac92a6d9d706f3bc80a
Rob Trehy <robert.trehy@outlook.com>
2022-06-01 15:34:01 +0100

    Remvoe WebApp Context

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,21 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { WebApps, WebAppsUX } from 'webapps-react';
+import { WebAppsUX } from 'webapps-react';
 
 import { Settings, Staff, Views } from './components';
 
 ReactDOM.render(
     <WebAppsUX>
-        <WebApps>
-            <BrowserRouter basename="/apps/StaffDirectory">
-                <Switch>
-                    <Route exact path="/views" name="Manage Views" component={Views} />
-                    <Route exact path="/staff" name="Manage Staff" component={Staff} />
-                    <Route path="/settings" name="App Settings" component={Settings} />
-                </Switch>
-            </BrowserRouter>
-        </WebApps>
+        <BrowserRouter basename="/apps/StaffDirectory">
+            <Switch>
+                <Route exact path="/views" name="Manage Views" component={Views} />
+                <Route exact path="/staff" name="Manage Staff" component={Staff} />
+                <Route path="/settings" name="App Settings" component={Settings} />
+            </Switch>
+        </BrowserRouter>
     </WebAppsUX>,
     document.getElementById('WebApps_AppContainer')
 );
